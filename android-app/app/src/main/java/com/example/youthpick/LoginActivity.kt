e8b3d1b25397f414a0571b8bf0d865f9f4ec59c1
giovannijunseokim <kingjunseo@gmail.com>
2022-10-16 21:53:12 +0900

    [UI] 로그인 화면 바꾸는중

--- android-app/app/src/main/java/com/example/youthpick/LoginActivity.kt
+++ android-app/app/src/main/java/com/example/youthpick/LoginActivity.kt
@@ -2,10 +2,12 @@ package com.example.youthpick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.youthpick.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
     }
 }
