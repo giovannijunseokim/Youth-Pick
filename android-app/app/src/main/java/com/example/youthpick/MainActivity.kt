084d02815db9ca88eab1d3e173ba4d672f48571f
giovannijunseokim <kingjunseo@gmail.com>
2022-10-31 15:51:38 +0900

    [UI] 드로어 버튼, 유저 버튼 fragment_main.xml 에서 activity_main.xml로 옮김 그에 맞게 함수도 수정

--- android-app/app/src/main/java/com/example/youthpick/MainActivity.kt
+++ android-app/app/src/main/java/com/example/youthpick/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import com.example.youthpick.databinding.ActivityMainBinding
 import com.example.youthpick.databinding.FragmentMainBinding
+import com.example.youthpick.databinding.ItemPagesBinding
 import com.example.youthpick.fragments.CalendarFragment
 import com.example.youthpick.fragments.ChatbotFragment
 import com.example.youthpick.fragments.MainFragment
@@ -14,6 +16,7 @@ import com.example.youthpick.fragments.NoteFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    lateinit var itemPagesBinding: ItemPagesBinding
     lateinit var fragmentMainBinding: FragmentMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +24,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         firstFragment()
         binding.bnvMain.setOnItemSelectedListener { item ->
-            changeFragment(item)
+            changeFragment(item.itemId)
         }
+
+        binding.btnDrawerOpener.setOnClickListener {
+            drawerOpen()
+        }
+
+        itemPagesBinding = ItemPagesBinding.inflate(layoutInflater)
+        itemPagesBinding.pagerItem.setOnClickListener {
+            when(itemPagesBinding.tvPagerTitle.text){
+                "유스픽 캘린더 바로가기" -> {
+                    Toast.makeText(this, "캘린더 화면으로 이동", Toast.LENGTH_SHORT).show()
+                    changeFragment(R.id.item_calendar)}
+                "유스픽 챗봇 바로가기" ->
+                    changeFragment(R.id.item_chatbot)
+                "메모 바로가기" ->
+                    changeFragment(R.id.item_note)
+                else ->
+                    Toast.makeText(this, "오류 발생", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
     private fun firstFragment(){
@@ -33,8 +56,8 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
     }
-    private fun changeFragment(item : MenuItem) : Boolean{
-        when(item.itemId) {
+    fun changeFragment(itemId : Int) : Boolean{
+        when(itemId) {
             R.id.item_main -> {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container_view, MainFragment())
--- android-app/app/src/main/java/com/example/youthpick/fragments/MainFragment.kt
+++ android-app/app/src/main/java/com/example/youthpick/fragments/MainFragment.kt
@@ -1,14 +1,15 @@
 package com.example.youthpick.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.youthpick.MainActivity
+import com.example.youthpick.R
 import com.example.youthpick.adapter.ViewPagerAdapter
 import com.example.youthpick.databinding.FragmentMainBinding
+import com.example.youthpick.databinding.ItemPagesBinding
 import com.example.youthpick.models.PageItem
 import com.example.youthpick.models.PagerViewModel
 
@@ -17,6 +18,7 @@ class MainFragment ():Fragment(){
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding
         get() = requireNotNull(_binding){"binding이 널임"}
+    private var itemPagesBinding : ItemPagesBinding? = null
     private val adapter by lazy { ViewPagerAdapter(requireContext()) }
 
     override fun onCreateView(
@@ -28,21 +30,28 @@ class MainFragment ():Fragment(){
 
         binding.viewpager.adapter = adapter
         binding.viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        binding.viewpager.setPadding(200, 0, 200, 0)
+        binding.viewpager.setPadding(240, 0, 240, 0)
         binding.viewpager.setPageTransformer { page, position ->
             page.translationX = position * 150
         }
 
-        binding.btnDrawerOpener.setOnClickListener {
-            (activity as MainActivity).drawerOpen()
+
+        binding.viewpager.setOnClickListener{
+            itemPagesBinding = ItemPagesBinding.inflate(layoutInflater)
+            when(itemPagesBinding!!.tvPagerTitle.text){
+                "유스픽 캘린더 바로가기" -> {
+                    Toast.makeText(context, "캘린더 화면으로 이동", Toast.LENGTH_SHORT).show()
+                    (activity as MainActivity).changeFragment(R.id.item_calendar)}
+                "유스픽 챗봇 바로가기" ->
+                    (activity as MainActivity).changeFragment(R.id.item_chatbot)
+                "메모 바로가기" ->
+                    (activity as MainActivity).changeFragment(R.id.item_note)
+                else ->
+                    Toast.makeText(context, "오류 발생", Toast.LENGTH_SHORT).show()
+            }
         }
 
         return binding.root
-    }
-
-    private fun getItemList(): List<PageItem> {
-        val pagerViewModel = PagerViewModel()
-        return pagerViewModel.itemList
     }
 
     override fun onDestroyView() {
