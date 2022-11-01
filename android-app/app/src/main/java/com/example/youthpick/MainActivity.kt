fd69137a52ea318f282e4a1c7a3c61c4b746df2c
giovannijunseokim <kingjunseo@gmail.com>
2022-11-01 20:32:00 +0900

    [feat] 네비게이션 아이템 클릭시 화면 전환 이벤트 추가

--- android-app/app/src/main/java/com/example/youthpick/MainActivity.kt
+++ android-app/app/src/main/java/com/example/youthpick/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.youthpick
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.Gravity
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.youthpick.databinding.ActivityMainBinding
-import com.example.youthpick.databinding.FragmentMainBinding
-import com.example.youthpick.databinding.ItemPagesBinding
 import com.example.youthpick.fragments.CalendarFragment
 import com.example.youthpick.fragments.ChatbotFragment
 import com.example.youthpick.fragments.MainFragment
@@ -16,8 +12,7 @@ import com.example.youthpick.fragments.NoteFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
-    lateinit var itemPagesBinding: ItemPagesBinding
-    lateinit var fragmentMainBinding: FragmentMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,19 +26,17 @@ class MainActivity : AppCompatActivity() {
             drawerOpen()
         }
 
-        itemPagesBinding = ItemPagesBinding.inflate(layoutInflater)
-        itemPagesBinding.pagerItem.setOnClickListener {
-            when(itemPagesBinding.tvPagerTitle.text){
-                "유스픽 캘린더 바로가기" -> {
-                    Toast.makeText(this, "캘린더 화면으로 이동", Toast.LENGTH_SHORT).show()
-                    changeFragment(R.id.item_calendar)}
-                "유스픽 챗봇 바로가기" ->
-                    changeFragment(R.id.item_chatbot)
-                "메모 바로가기" ->
-                    changeFragment(R.id.item_note)
-                else ->
-                    Toast.makeText(this, "오류 발생", Toast.LENGTH_SHORT).show()
-            }
+        binding.tvDrawerCalendar.setOnClickListener {
+            changeFragment(R.id.item_calendar)
+            binding.bnvMain.selectedItemId = R.id.item_calendar
+        }
+        binding.tvDrawerChatbot.setOnClickListener {
+            changeFragment(R.id.item_chatbot)
+            binding.bnvMain.selectedItemId = R.id.item_chatbot
+        }
+        binding.tvDrawerNote.setOnClickListener {
+            changeFragment(R.id.item_note)
+            binding.bnvMain.selectedItemId = R.id.item_note
         }
 
     }
--- android-app/app/src/main/java/com/example/youthpick/fragments/MainFragment.kt
+++ android-app/app/src/main/java/com/example/youthpick/fragments/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.youthpick.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -18,7 +19,7 @@ class MainFragment ():Fragment(){
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding
         get() = requireNotNull(_binding){"binding이 널임"}
-    private var itemPagesBinding : ItemPagesBinding? = null
+    lateinit var itemPagesBinding: ItemPagesBinding
     private val adapter by lazy { ViewPagerAdapter(requireContext()) }
 
     override fun onCreateView(
@@ -35,21 +36,24 @@ class MainFragment ():Fragment(){
             page.translationX = position * 150
         }
 
-
-        binding.viewpager.setOnClickListener{
-            itemPagesBinding = ItemPagesBinding.inflate(layoutInflater)
-            when(itemPagesBinding!!.tvPagerTitle.text){
-                "유스픽 캘린더 바로가기" -> {
-                    Toast.makeText(context, "캘린더 화면으로 이동", Toast.LENGTH_SHORT).show()
-                    (activity as MainActivity).changeFragment(R.id.item_calendar)}
-                "유스픽 챗봇 바로가기" ->
-                    (activity as MainActivity).changeFragment(R.id.item_chatbot)
-                "메모 바로가기" ->
-                    (activity as MainActivity).changeFragment(R.id.item_note)
-                else ->
-                    Toast.makeText(context, "오류 발생", Toast.LENGTH_SHORT).show()
-            }
-        }
+//        binding.viewpager.setOnClickListener{
+//            Log.d("GIO", "No,,,,")
+//            itemPagesBinding = ItemPagesBinding.inflate(layoutInflater)
+//            itemPagesBinding.pagerItem.setOnClickListener {
+//                when(itemPagesBinding.tvPagerTitle.text){
+//                    "유스픽 캘린더 바로가기" -> {
+//                        Log.d("GIO", "No,,,,")
+//                        (activity as MainActivity).changeFragment(R.id.item_calendar)}
+//                    "유스픽 챗봇 바로가기" ->
+//                        (activity as MainActivity).changeFragment(R.id.item_chatbot)
+//                    "메모 바로가기" ->
+//                        (activity as MainActivity).changeFragment(R.id.item_note)
+//                    else ->
+//                        Log.d("GIO", "No,,,,")
+//                }
+//            }
+//
+//        }
 
         return binding.root
     }
