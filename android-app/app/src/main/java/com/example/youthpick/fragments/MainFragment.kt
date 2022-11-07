cbed5ff710c40fa1366e8cb094d47bcda1a2c33d
giovannijunseokim <kingjunseo@gmail.com>
2022-11-07 19:41:45 +0900

    [UI] 홈 프래그먼트 뷰페이저 인디케이터 추가

--- android-app/app/src/main/java/com/example/youthpick/fragments/MainFragment.kt
+++ android-app/app/src/main/java/com/example/youthpick/fragments/MainFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.youthpick.MainActivity
 import com.example.youthpick.R
@@ -13,6 +14,7 @@ import com.example.youthpick.databinding.FragmentMainBinding
 import com.example.youthpick.databinding.ItemPagesBinding
 import com.example.youthpick.models.PageItem
 import com.example.youthpick.models.PagerViewModel
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 
 
 class MainFragment ():Fragment(){
@@ -34,6 +36,9 @@ class MainFragment ():Fragment(){
         binding.viewpager.setPageTransformer { page, position ->
             page.translationX = position * 150
         }
+
+        val wormDotsIndicator = binding.wormDotsIndicator
+        wormDotsIndicator.attachTo(binding.viewpager)
 
         binding.btnDrawerOpener.setOnClickListener {
             (activity as MainActivity).drawerOpen()
