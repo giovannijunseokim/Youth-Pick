b5a3b87f76b2a2baa99ca245ef81df46633d523c
민종 <s2hoon326@gmail.com>
2022-11-09 00:13:25 +0900

    calender CSS

--- 캘린더/app.js
+++ 캘린더/app.js
@@ -4,12 +4,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
-  app.use(express.static('css'));
-  app.use(express.static('image'));
-  app.use(express.static('js'));
-  app.use(express.static('vendor'));
-  app.use(express.static('app.js'));
-
-  app.listen(3000, () => {
-    console.log("listening on *:3000");
+  app.use('/', (req, res) => express.static(__dirname, './css')); 
+  app.use('/', (req, res) => express.static(__dirname, './image'));
+  app.use('/', (req, res) => express.static(__dirname, './js'));
+  app.use('/', (req, res) => express.static(__dirname, './vendor'));   
+app.listen(3000, () => {
+  console.log("listening on *:3000");
 });
